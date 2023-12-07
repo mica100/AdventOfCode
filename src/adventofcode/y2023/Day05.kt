9359080a904824c0fe36ec4bd55dcd9538a4179a
Michael Augustin <Michael.Augustin@bmw.de>
2023-12-07 16:03:09 +0100

    2023 day 05

--- src/adventofcode/y2023/Day05.kt
+++ src/adventofcode/y2023/Day05.kt
@@ -1,26 +1,107 @@
 package adventofcode.y2023
 
-data class CalcMap(val name: String) {
-    val calc = mutableListOf<String>()
+data class CalcMap(val name: String, val from: String, val to: String) {
+    val calc = mutableListOf<ConversionMap>()
+
+    fun addConversion(line: String) {
+        calc.add(createConversionMap(line))
+    }
+
+    fun convert(value: Long) = calc.fold(value) { acc, c ->
+        c.convert(value) ?: acc
+    }
 }
+
+class ConversionMap(val destStart: Long, source: Long, rangeLength: Long) {
+    val sourceRange = source until source + rangeLength
+
+    fun convert(value: Long) =
+        if (sourceRange.contains(value)) value - sourceRange.first + destStart else null
+}
+
+fun createConversionMap(line: String): ConversionMap {
+    // 50 98 2 => dest, source, length
+    val (dest, source, length) = line.split(" ")
+    return ConversionMap(dest.toLong(), source.toLong(), length.toLong())
+}
+
+class CalcMaps(val maps: MutableMap<String, CalcMap>) {
+    val size: Int
+        get() = maps.size
+
+    val listTo: Map<String, String> = maps.values.associate { Pair(it.to, it.name) }
+    val listFrom: Map<String, String> = maps.values.associate { Pair(it.from, it.name) }
+
+    fun getConversionSteps(from: String = "location", to: String = "seed"): List<String> {
+        val calcList = mutableListOf<String>()
+        var lastElement: String? = from
+        while (lastElement != null && to != lastElement) {
+            val mapName = listTo.getOrDefault(lastElement, null)
+            if (mapName != null) {
+                calcList.add(mapName)
+                lastElement = mapName.fromMapName().first
+            } else lastElement = null
+        }
+        return calcList.reversed()
+    }
+
+    fun stepsAsMapList(steps: List<String>) = steps.mapNotNull { mapName -> maps[mapName] }
+
+    fun convert(value: Long, steps: List<CalcMap>): Long =
+        steps.fold(value) { acc, calcMap ->
+            calcMap.convert(acc)
+        }
+
+    fun getMinOf(seeds: List<Long>, from: String = "location", to: String = "seed"): Pair<Long, Long> {
+        val steps = getConversionSteps(from, to)
+        val stepsAsList = stepsAsMapList(steps)
+        val InOutPairs = seeds.map { Pair(it, convert(it, stepsAsList)) }
+        return InOutPairs.minByOrNull { it.second }!!
+    }
+
+    fun getMinOfSeedRanges(seedRanges: List<Long>): Pair<Long,Long> {
+        var minValue = Long.MAX_VALUE
+        var input = Long.MAX_VALUE
+        val steps = getConversionSteps()
+        val stepsAsList = stepsAsMapList(steps)
+        seedRanges.chunked(2) { (start, length) ->
+            for (i in start until start + length) {
+                val a = convert(i, stepsAsList)
+                if (a < minValue) {
+                    minValue = a
+                    input = i
+                }
+            }
+        }
+        return input to minValue
+    }
+
+}
+
+
+fun Pair<String, String>.asMapName() = "${first}-to-${second}"
+fun String.fromMapName() = split("-to-").let { Pair(it[0], it[1]) }
 
 class Day05(val inputList: List<String>) {
 
-    val maps: MutableMap<String, List<String>> = mutableMapOf()
+    val seeds = mutableListOf<Long>()
 
-    init {
+    val calcMaps by lazy {
+        val maps: MutableMap<String, CalcMap> = mutableMapOf()
         var currentMap: CalcMap? = null
 
         inputList.forEach { line ->
             if (line.endsWith(" map:")) {
                 val name = line.substringBeforeLast(" map:")
-                maps.getOrPut(name, ::emptyList)
+                val (from, to) = name.split("-to-")
+                currentMap = maps.getOrPut(name) { CalcMap(name, from, to) }
             } else if (line.startsWith("seeds: ")) {
-                maps.getOrPut("seeds") { line.split(":").drop(1)}
+                seeds.addAll(line.substringAfter("seeds: ").split(" ").map { it.toLong() })
+            } else if (line.isNotBlank()) {
+                currentMap?.addConversion(line)
             }
         }
-
+        CalcMaps(maps)
     }
-
 
 }
--- test/adventofcode/y2023/Day05Test.kt
+++ test/adventofcode/y2023/Day05Test.kt
@@ -1,5 +1,6 @@
 package adventofcode.y2023
 
+import adventofcode.readInput
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -45,7 +46,64 @@ class Day05Test {
     fun testBasics() {
         val subject = Day05(testInput.reader().readLines())
 
-        println(subject.maps)
-        assertEquals(8, subject.maps.size)
+        println(subject.calcMaps)
+        assertEquals(7, subject.calcMaps.size)
+        assertEquals(true, subject.seeds.contains(79))
+
+        val calcMaps = subject.calcMaps
+        println(calcMaps.listTo)
+        assertEquals(7, calcMaps.listTo.size)
+        println(calcMaps.getConversionSteps())
+        assertEquals("soil-to-fertilizer", calcMaps.getConversionSteps()[1])
+
+        val steps = calcMaps.getConversionSteps()
+        val stepsAsList = calcMaps.stepsAsMapList(steps)
+
+        assertEquals(82L, subject.calcMaps.convert(79, stepsAsList))
+        assertEquals(43L, subject.calcMaps.convert(14, stepsAsList))
+        assertEquals(86L, subject.calcMaps.convert(55, stepsAsList))
+        assertEquals(35L, subject.calcMaps.convert(13, stepsAsList))
+
+        assertEquals(Pair(13L, 35L), subject.calcMaps.getMinOf(subject.seeds))
+
+        assertEquals(Pair(82L, 46L), subject.calcMaps.getMinOfSeedRanges(subject.seeds))
+    }
+
+    @Test
+    fun testConversionMap() {
+        val conversion: ConversionMap = createConversionMap("50 98 2")
+        assertEquals(false, conversion.sourceRange.contains(97))
+        assertEquals(true, conversion.sourceRange.contains(98))
+        assertEquals(true, conversion.sourceRange.contains(99))
+        assertEquals(false, conversion.sourceRange.contains(100))
+
+        assertEquals(50L, conversion.convert(98))
+        assertEquals(null, conversion.convert(49))
+    }
+
+    @Test
+    fun testCalcMap() {
+        val calc = CalcMap("from-to-target", "from", "target")
+        calc.addConversion("50 98 2")
+        calc.addConversion("52 50 48")
+
+        assertEquals(55L, calc.convert(53))
+        assertEquals(99L, calc.convert(97))
+        assertEquals(50L, calc.convert(98))
+        assertEquals(10L, calc.convert(10))
+    }
+
+    @Test
+    fun testSolution1() {
+        val subject = Day05(readInput(5, 2023))
+        assertEquals(Pair(1921754120L, 825516882L), subject.calcMaps.getMinOf(subject.seeds))
+
+    }
+
+    @Test
+    fun testSolution2() {
+        val subject = Day05(readInput(5, 2023))
+        assertEquals(Pair(3881100901L, 136096660L), subject.calcMaps.getMinOfSeedRanges(subject.seeds))
+        // runs 14min08sec
     }
 }
