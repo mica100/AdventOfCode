41f4011b971fcee4499ad76354bcfcdd30c4a614
mica <maugustin@gmx.net>
2024-12-05 19:21:36 +0100

    safe 2024 day 5

--- src/adventofcode/y2024/Day05.kt
+++ src/adventofcode/y2024/Day05.kt
@@ -1,5 +1,61 @@
 package adventofcode.y2024
 
+typealias Page = Int
+data class Rule(val a: Page, val b: Page)
+typealias Update = List<Page>
 
 class Day05(inputData: List<String>) {
+    private val splitIndex = inputData.indexOf("")
+    val rules: List<Rule> = inputData.take(splitIndex).map { line -> line.split("|").let { Rule(it[0].toInt(), it[1].toInt()) } }
+    val updates: List<List<Page>> = inputData.drop(splitIndex+1).map { line -> line.split(',').map { it.toInt() } }
+
+    fun getRules(pages: List<Page>): List<Rule> = rules.filter { pages.contains(it.a) || pages.contains(it.b) }
+
+    fun isOrdered(update: Update): Boolean {
+        return getRules(update).all { rule -> !update.contains(rule.a) || !update.contains(rule.b) || update.indexOf(rule.a) < update.indexOf(rule.b) }
+    }
+    fun failedRules(update: Update): List<Rule> {
+        return getRules(update).filter { rule -> update.contains(rule.a) && update.contains(rule.b) && update.indexOf(rule.a) > update.indexOf(rule.b) }
+    }
+
+    fun middlePage(update: Update): Int = update[(update.size -1) / 2]
+
+    fun sumMiddlePages(): Int =
+        updates.mapIndexed { index, update ->
+            if (isOrdered(update)) {
+                middlePage(update)
+            } else 0
+        }.sum()
+
+//    fun sumMiddlePagesFromIncorrect(): Int =
+//        updates.mapIndexed { index, update ->
+//            if (!isOrdered(update)) {
+//                val updateNew = update.toMutableList()
+//                var previousErrors:  List<Rule> = emptyList()
+//                for (attemp in 0..50) {
+//                    val failedRules = failedRules(updateNew)
+//                    if (failedRules.isEmpty()) {
+//                        println("$index $attemp fixed!")
+//                        break
+//                    }
+//                    if (failedRules == previousErrors) {
+//                        println("$index $attemp failed: no changes on errors")
+//                        break
+//                    }
+//                    previousErrors = failedRules
+//
+//                    println("$index $attemp: failed: $updateNew, rules: ${failedRules.size} $failedRules")
+//                    failedRules.forEach { rule ->
+//                        val index1 = updateNew.indexOf(rule.a)
+//                        val index2 = updateNew.indexOf(rule.b)
+//                        val element = updateNew[index1]
+//                        updateNew.remove(element)
+//                        updateNew.add(index2, element)
+//                    }
+//
+//                }
+//                middlePage(updateNew)
+//            } else 0
+//        }.sum()
+
 }
--- test/adventofcode/y2024/Day05Test.kt
+++ test/adventofcode/y2024/Day05Test.kt
@@ -3,31 +3,71 @@ package adventofcode.y2024
 import adventofcode.readInput
 import adventofcode.readInputData
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 
 class Day05Test : FunSpec() {
 
     val testData = """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+        
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
     """.trimIndent()
 
     init {
         test("check example") {
-            val day05 = Day05(readInputData(testData))
+            val d = Day05(readInputData(testData))
+            d.rules.size shouldBe 21
+            d.rules[20] shouldBe Rule(53, 13)
+            d.updates.size shouldBe 6
+            d.getRules(listOf(75)).size shouldBe 6
+            for (updateNr in 0.. 2) {
+                d.isOrdered(d.updates[updateNr]) shouldBe true
+            }
+            for (updateNr in 3.. d.updates.size-1) {
+                d.isOrdered(d.updates[updateNr]) shouldBe false
+            }
+            d.middlePage(d.updates[0]) shouldBe 61
+            d.sumMiddlePages() shouldBe 143
         }
 
         test("puzzle 1") {
-            val day05 = Day05(readInput(5, 2024))
-            //day05.countAll() shouldBe 2532
+            val d = Day05(readInput(5, 2024))
+            d.sumMiddlePages() shouldBe 5064
         }
 
         test("example 2") {
-            val testData = """
-            """.trimIndent()
-
-            val day05 = Day05(readInputData(testData))
+            val d = Day05(readInputData(testData))
+            //d.sumMiddlePagesFromIncorrect() shouldBe 123
         }
 
         test("puzzle 2") {
-            val day05 = Day05(readInput(5, 2024))
+            val d = Day05(readInput(5, 2024))
+            //d.sumMiddlePagesFromIncorrect() shouldBe 100
         }
     }
 
